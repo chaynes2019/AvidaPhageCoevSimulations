3b1155c7459393a722c66ab0f39abd93917b37a3
Cameron Haynes <chaynes2019@gmail.com>
2024-02-19 17:12:35 -0500

    Fixed resetSpopMaker and updated bash scripts

--- experiments/2024-2-15-HPCCTest/config/resetSpopFileMaker.py
+++ experiments/2024-2-15-HPCCTest/config/resetSpopFileMaker.py
@@ -13,8 +13,23 @@ with open('parasiteLines.txt', 'r') as f:
             for position in positions.split(','):
                 parasitePositions.append(position)
             #This parasite is extinct, and thus it shouldn't be included or else it... well, am I right?
+                
+hostPositions = []
+with open('hostLines.txt', 'r') as f:
+    hostLines = f.readlines()
+    for host in hostLines:
+        hostEnumerated = host.split()
+        if (len(hostEnumerated) >= 18):
+            #print(parasite)
+            #This parasite is still extant because it has some data on what cells it's present at
+            positions = hostEnumerated[17]
+            #Parses string of multiple positions for the parasite genotypes with multiple copies
+            for position in positions.split(','):
+                hostPositions.append(int(position))
+            #This parasite is extinct, and thus it shouldn't be included or else it... well, am I right?
 
-
+hostPositions = sorted(hostPositions)
+#print(hostPositions)
 #print(parasiteLines)
 
 preamble = []
@@ -29,8 +44,15 @@ numberOfParasites = len(parasitePositions)
 with open('resetSpop.spop', 'w') as f:
     f.writelines(preamble)
     f.writelines(["\n"])
-    hostlines = ["1 div:ext (none) (none) 0 1 320 0 0 0 0 -1 -1 0 2 transsmt ycdAoaddxccccccccccccccEEEcccccccccccccccccccccccccccccccccccccccccccccccccccgccccccccccccccccccccccccccccccccccccccccEccccDcccccccccccccccccccccccccccccccccccccccccccccccccccdccccccccccccccccccccccccccccccccccccccccccxcccccccccccccccccccccccccccccccccccccccccccccccccAcccccccccccccccccccucccccccccccccccccccccypqvcrGxab 0 0 0 \n"]
-    for k in range(3600):
-        hostlines.append("{} div:int (none) {} 1 1 320 0 0 0 0 -1 -1 0 2 transsmt ycdAoaddxccccccccccccccEEEcccccccccccccccccccccccccccccccccccccccccccccccccccgccccccccccccccccccccccccccccccccccccccccEccccDcccccccccccccccccccccccccccccccccccccccccccccccccccdccccccccccccccccccccccccccccccccccccccccccxcccccccccccccccccccccccccccccccccccccccccccccccccAcccccccccccccccccccucccccccccccccccccccccypqvcrGxab {} 0 0 \n".format(k + 2, k + 1, k))
+    for k in range(len(hostPositions)):
+        hostPositions[k] = str(hostPositions[k])
+    
+    gestationOffsetAndLineageLabels = ["0" for k in range(len(hostPositions))]
+    hostlines = ["1 div:ext (none) (none) 0 1 320 0 0 0 0 -1 -1 0 2 transsmt ycdAoaddxccccccccccccccEEEcccccccccccccccccccccccccccccccccccccccccccccccccccgccccccccccccccccccccccccccccccccccccccccEccccDcccccccccccccccccccccccccccccccccccccccccccccccccccdccccccccccccccccccccccccccccccccccccccccccxcccccccccccccccccccccccccccccccccccccccccccccccccAcccccccccccccccccccucccccccccccccccccccccypqvcrGxab \n".format(",".join(hostPositions), ",".join(gestationOffsetAndLineageLabels), ",".join(gestationOffsetAndLineageLabels))]
+    hostlines.append("2 div:int (none) 1 {} {} 320 0 0 0 0 -1 -1 0 2 transsmt ycdAoaddxccccccccccccccEEEcccccccccccccccccccccccccccccccccccccccccccccccccccgccccccccccccccccccccccccccccccccccccccccEccccDcccccccccccccccccccccccccccccccccccccccccccccccccccdccccccccccccccccccccccccccccccccccccccccccxcccccccccccccccccccccccccccccccccccccccccccccccccAcccccccccccccccccccucccccccccccccccccccccypqvcrGxab {} {} {} \n".format(len(hostPositions), len(hostPositions), ",".join(hostPositions), ",".join(gestationOffsetAndLineageLabels), ",".join(gestationOffsetAndLineageLabels)))
+
     f.writelines(hostlines)
     f.writelines(parasiteLines)
+
+extraPositions = [pos for pos in parasitePositions if pos not in hostPositions]
+print(f"Parasites without a home: {extraPositions}")
--- experiments/ParasiteTest/hpcc/LocalSubmissionScripts/runResettingParasiteHostCoev.sh
+++ experiments/ParasiteTest/hpcc/LocalSubmissionScripts/runResettingParasiteHostCoev.sh
@@ -1,43 +1,7 @@
-#!/bin/bash
-# The interpreter used to execute the script
+OVERALL_RUN_LENGTH=10000
+RESET_INTERVAL=5000
 
-
-#SBATCH --job-name=parasiteHostCoevTest
-#SBATCH --mail-user=clhaynes@umich.edu
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --cpus-per-task=1
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=1000m 
-#SBATCH --time=00-10:00:00
-#SBATCH --account=zamanlh0
-#SBATCH --partition=standard
-#SBATCH --output=/home/%u/%x-%j.log
-
-USERNAME=clhaynes
-EXPERIMENT_ID=2024-2-15-HPCCTest
-OUTPUT_DIR=/scratch/zamanlh_root/zamanlh0/${USERNAME}/${EXPERIMENT_ID}
-CONFIG_DIR=/home/${USERNAME}/Documents/AvidaPhageCoevSimulations/experiments/${EXPERIMENT_ID}/config
-#SEED_OFFSET=2100
-
-#SEED=$((SEED_OFFSET + SLURM_ARRAY_TASK_ID - 1))
-#JOB_ID=${SLURM_ARRAY_TASK_ID}
-RUN_DIR=${OUTPUT_DIR}
-
-mkdir -p ${RUN_DIR}
-cd ${RUN_DIR}
-mkdir AnalysisScripts
-mkdir config
-
-cd ${CONFIG_DIR}
-cp * ${RUN_DIR}/config
-cd ../AnalysisScripts
-cp * ${RUN_DIR}/AnalysisScripts
-
-cd ${RUN_DIR}/config
-
-TOTAL_RUN_LENGTH=72000
-RESET_INTERVAL=24000
+cd ../config
 
 ./avida -s 30 -set EVENT_FILE eventsBeginCoev.cfg
 
