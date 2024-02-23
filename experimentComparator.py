a28bed98d9ff1fd06712a67bcd6f3abb6455874d
Cameron Haynes <chaynes2019@gmail.com>
2024-02-23 16:02:30 -0500

    Updated HPCC Template of phenotypeTasksGetter.py and update HPCC experimentComparator

--- experimentComparator.py
+++ experimentComparator.py
@@ -6,11 +6,11 @@ experiment1 = input("Enter the name of Experiment 1: ")
 experiment2 = input("Enter the name of Experiment 2: ")
 overallRunLength = input("How many updates were the experiments run for?: ")
 
-experiment1TasksDataframe = pd.read_csv(f"/OutputData/{experiment1}-AverageNumTasksVsUpdates.csv")
-experiment2TasksDataframe = pd.read_csv(f"../OutputData/{experiment2}-AverageNumTasksVsUpdates.csv")
+experiment1TasksDataframe = pd.read_csv(f"experiments/{experiment1}/OutputData/{experiment1}-AverageNumTasksVsUpdates.csv")
+experiment2TasksDataframe = pd.read_csv(f"experiments/{experiment2}/OutputData/{experiment2}-AverageNumTasksVsUpdates.csv")
 '''
-experiment1LengthDataframe = pd.read_csv(f"../OutputData/{experiment1}-avgParasiteGenomeLengthVsUpdates.csv")
-experiment2LengthDataframe = pd.read_csv(f"../OutputData/{experiment2}-avgParasiteGenomeLengthVsUpdates.csv")
+experiment1LengthDataframe = pd.read_csv(f"experiments/{experiment1}/OutputData/{experiment1}-avgParasiteGenomeLengthVsUpdates.csv")
+experiment2LengthDataframe = pd.read_csv(f"experiments/{experiment2}/OutputData/{experiment2}-avgParasiteGenomeLengthVsUpdates.csv")
 '''
 experiment1Tasks = experiment1TasksDataframe.to_numpy()
 experiment2Tasks = experiment2TasksDataframe.to_numpy()
@@ -24,14 +24,15 @@ print(f"Tasks in experiment 1: {experiment1Tasks}")
 
 plt.figure(0)
 plt.plot(np.array([k * 100 for k in range(int(overallRunLength) // 100 + 1)]),
-         experiment1Tasks[:, 1])
+         experiment1Tasks[:, 1], label="Reset Interval = 24000")
 plt.plot(np.array([k * 100 for k in range(int(overallRunLength) // 100 + 1)]),
-         experiment2Tasks[:, 1])
+         experiment2Tasks[:, 1], label="No Reset Control")
+plt.legend()
 '''
 plt.figure(1)
 plt.plot(np.array([k * 100 for k in range(overallRunLength // 100 + 1)]),
          experiment1Lengths, experiment2Lengths)
 '''
-plt.show()
+plt.savefig("AvgNumTasksVsUpdatesExperimentVsControl")
 
 
